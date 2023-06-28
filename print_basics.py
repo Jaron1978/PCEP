3f82235729ab11b8f773a83f02723da29cc3ff43
Ron Jackson <ron.jackson@amobee.com>
2023-06-28 09:52:13 +0100

    Added print_basics

---
+++ print_basics.py
@@ -0,0 +1,6 @@
+# This is some basic Python code using everything Ive learnt in the last couple of lessons. I will be adding Comments to show what Ive leanrt and what the code does. 
+
+print('This is a brief line of code showcasing what Ive picked up so far.\nThis will include a line break and I\'m also going to showcase the escape charecter. ')
+
+# The code above prints the the line of text or string. The line break (\n) literally breaks the line and moves the second line of text to a new line.
+# The escape (\) allows me to use the ' without impacting the string. 
