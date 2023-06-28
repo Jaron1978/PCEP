3dfe5d42afdf722be3cd62ad630bbe7ff3df283d
Ron Jackson <ron.jackson@amobee.com>
2023-06-28 12:57:35 +0100

    Added Variables file

---
+++ 3. Variables.py
@@ -0,0 +1,11 @@
+# A named place in Memory where you can store data for future use. 
+# A Variable has a name and a value. 
+
+greeting  = 'Hello!'
+print(greeting)
+greeting = 'Hi, Everybody'
+print(greeting)
+
+# In the code snippet above we have created a variable called greeting. When ever the greeting variable is called in this instance, it will print Hello!
+# Note that there are no apostraphies. If there were, it would print greeting which would be a string. 
+# Variable names must start with a letter or underscore. Upper and Lowercase letters are different, Python also has specified Variables that you cannot use. 
