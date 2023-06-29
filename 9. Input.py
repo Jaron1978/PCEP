834abb894875164244ae7530e59fb83552388001
Ron Jackson <ron.jackson@amobee.com>
2023-06-29 11:32:44 +0100

    Added input.py

---
+++ 9. Input.py
@@ -0,0 +1,7 @@
+# The input function asks for a value and gives you back a response. 
+print('Please Enter your name: ')
+user_name = input()
+print('Hello', user_name)
+
+# Functions in python can 1. Cause some effect, 2. Return a value. 
+# Input always returns a string value. 
