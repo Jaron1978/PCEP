387ca34846cd10570701f484347b4030144df038
Ron Jackson <ron.jackson@amobee.com>
2023-06-29 09:28:06 +0100

    Updated reassign

---
+++ 8. Reassign_values.py
@@ -0,0 +1,26 @@
+# age = 28 -> Comment out to use code later on. 
+# print(age) 
+
+# This will print 28
+
+# new_age = age + 2 -> Comment out to use code later on. 
+
+# Printing new_age will be 30. This is age + 2. 
+
+# age = 28
+# age = age +7
+# print(age)
+
+# The above line of code can also be written as: 
+
+# age = 28
+# age += 7 
+# print(age) # The += is adding the origional age 28 to the 7 to equal 35. 
+
+# age *= 2 is age times 2, age -= 5 means subtract 5, age /= 3 is current value divided by 3
+
+# String concatenation means to join strings: 
+# text = 'hokus ' + 'pokus'
+# print(text)
+
+print('Hello ' * 5) # Prints Hello 5 times. 
