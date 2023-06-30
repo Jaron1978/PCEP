949c3b9ca1c4937ef328301bc210a06f683c272b
Ron Jackson <ron.jackson@amobee.com>
2023-06-30 11:27:14 +0100

    inout_basics created

---
+++ input_basics.py
@@ -0,0 +1,5 @@
+# A short program where the user is asked to enter their name. The Users name is then printed in the Welcome message.
+
+print('Please Enter your name: ')
+user_name = input()
+print('Welcome', user_name)
