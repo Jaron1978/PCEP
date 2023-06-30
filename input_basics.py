6e3b7b2922bbf6add1b5ba3fd5dc7ad6db3179ac
Ron Jackson <ron.jackson@amobee.com>
2023-06-30 11:35:17 +0100

    Updated inout_basics

--- input_basics.py
+++ input_basics.py
@@ -1,5 +1,6 @@
-# A short program where the user is asked to enter their name. The Users name is then printed in the Welcome message.
+# A short program where the user is asked to enter their name, email address and telephone number. 
 
-print('Please Enter your name: ')
-user_name = input()
-print('Welcome', user_name)
+user_name = input('Please Enter your Full Name: ')
+email = input('Please Enter your Full e-Mail address: ')
+phone = input('Please Enter your contact Telephone Number: ')
+print('Welcome', user_name,'Confirming that your eMail address is ',email, 'and you can be contacted on the following Number', phone)
