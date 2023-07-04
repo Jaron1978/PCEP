af67997d156de15f532901343936e78b581a04fe
Ron Jackson <ron.jackson@amobee.com>
2023-07-04 16:28:16 +0100

    New files

---
+++ 11. more_operators.py
@@ -0,0 +1,13 @@
+# Unary Operator:
+# print(+12) # This will print 12
+
+# The order of Operators: 
+# 1. **
+# 2. * / // %
+# 3. + -
+
+print(2 + 3 * 2) # Starts with 3 X 2 then adds 2
+print(2 + 3)* 2 # This equals 10. 2 + 3 = 5, X by 2. 
+
+# Exponentation Operator ** always starts from the right. 
+# 2 ** 2 ** 3 = 256 ( 2  ** 3 = 8, then 2 ** 8 = 256)
---
+++ 12. more_print_and_strings.py
@@ -0,0 +1,6 @@
+# len() function returns the length of charecters in a string: 
+# len('Hello User')
+
+# Keyword Arguments:
+print('Hello World!', end='.')
+print('This is earth speaking', 'Welcome!', sep='-')
