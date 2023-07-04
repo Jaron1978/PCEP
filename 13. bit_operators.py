f374ce8aa7bafa7097ac739a58e93c6532d1b341
Ron Jackson <ron.jackson@amobee.com>
2023-07-04 16:27:23 +0100

    Created three new files.

---
+++ 13. bit_operators.py
@@ -0,0 +1,36 @@
+# bitwise operators are rarely used in Python, but are part of the exam syllabus. 
+
+# 6 Bitwise Operators. 
+# &, |, ~, ^, <<, >>
+
+first_bit = 1
+second_bit = 0
+
+print(first_bit & second_bit)
+# This will equal 0. 1 + 0 is 0
+
+first_bit = 1
+second_bit = 0
+
+print(first_bit | second_bit)
+# This will equal 1. | represents Or. As long as one of the bits is 1. 
+
+first_bit = 1
+second_bit = 0
+
+print(first_bit ^ second_bit)
+# Exclusive Or. If 1 of them is 0, its 1. If both are 0, its 0. 
+
+first_bit = 1
+second_bit = 0
+
+print(first_bit ~ second_bit)
+# Tilda is a logical negation. In front of a 1 = -2
+
+
+
+
+
+
+
+
